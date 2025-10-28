37f85e3ef9c0568972b218a27066944bdf987d72
tgenz1213 <timothy.genz@yahoo.com>
2025-10-28 11:05:14 -0500

    fix(deps): use Image default import

--- app/onboarding/welcome/page.js
+++ app/onboarding/welcome/page.js
@@ -1,5 +1,5 @@
 'use client';
-import { Image } from 'next/image';
+import Image from 'next/image';
 import { useRouter } from 'next/navigation';
 import { useState, useEffect, useCallback } from 'react';
 import toast from 'react-hot-toast';
